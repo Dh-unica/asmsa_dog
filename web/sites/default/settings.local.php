4eb8506be6d62586746889157bece571dc0b6218
Alessandro Capra <alessandrocapra@gmail.com>
2025-05-25 10:09:00 +0200

    Implemento cache bin permanent

---
+++ web/sites/default/settings.local.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Local development settings.
+ */
+
+// Configurazione delle cache bin permanenti.
+$settings["cache"]["bins"]["omeka_resources"] = "cache.backend.permanent_database";
+$settings["cache"]["bins"]["omeka_geo_data"] = "cache.backend.permanent_database";
+
